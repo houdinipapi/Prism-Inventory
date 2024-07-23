995696ddfa7fd054c4be84080fffdd631f133501
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-23 16:18:14 +0300

    feat: Add Wine model to manage wine inventory
    The code changes include adding a new model called "Wine" to manage the wine inventory. The model has fields for name, category, and quantity. This will allow the application to store and retrieve information about different wines in the inventory.
    
    Note: The recent user commits and repository commits do not provide any additional relevant information for crafting the commit message.

--- wine_inventory/wines/models.py
+++ wine_inventory/wines/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Wine(models.Model):
+    name= models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
