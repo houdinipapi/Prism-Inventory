7130b9018eb113f0db754564a9d2436e49f1fc04
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-23 16:57:22 +0300

    feat: Add wine update and delete functionality
    This commit adds the necessary code changes to enable updating and deleting wine entries. It includes the implementation of the `wine_update` and `wine_delete` views in the `views.py` file. The corresponding URL patterns are also added in the `urls.py` file. Now users can update and delete wine entries from the wine inventory.

--- wine_inventory/wines/urls.py
+++ wine_inventory/wines/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.wine_list, name='wine_list'),
     path('wine/<int:pk>/', views.wine_detail, name='wine_detail'),
     path('wine/new/', views.wine_create, name='wine_create'),
+    path('wine/<int:pk>/edit/', views.wine_update, name='wine_update'),
+    path('wine/<int:pk>/delete/', views.wine_delete, name='wine_delete'),
 ]
--- wine_inventory/wines/views.py
+++ wine_inventory/wines/views.py
@@ -34,3 +34,32 @@ def wine_create(request):
             'wines/wine_form.html',
             {'form': form}
         )
+    
+
+def wine_update(request, pk):
+    wine = get_object_or_404(Wine, pk=pk)
+    if request.method == "POST":
+        form = WineForm(request.POST, instance=wine)
+        if form.is_valid():
+            form.save()
+            # return redirect('wine_detail', pk=wine.pk)
+            return redirect("wine_list")
+    else:
+        form = WineForm(instance=wine)
+    return render(
+        request,
+        'wines/wine_form.html',
+        {'form': form}
+    )
+
+
+def wine_delete(request, pk):
+    wine = get_object_or_404(Wine, pk=pk)
+    if request.method == "POST":
+        wine.delete()
+        return redirect('wine_list')
+    return render(
+        request,
+        'wines/wine_confirm_delete.html',
+        {'wine': wine}
+    )
