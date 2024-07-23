355560acd3e1de0fdb146045205bb01ac0123961
Houdini Papi <ochiengcliff.co@gmail.com>
2024-07-23 22:39:02 +0300

    feat: Add homepage template and URL
    This commit adds the homepage template and URL to the wine inventory system. The template includes a welcome message and links to the wine list and outlet list pages. This provides users with a starting point and easy access to the main features of the application.

--- wine_inventory/wine_inventory/urls.py
+++ wine_inventory/wine_inventory/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home, name="home"),
     path("wines/", include("wines.urls")),
     path("outlets/", include("outlets.urls")),
 ]
